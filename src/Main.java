9fee857a20a8c18513a60b97203db924e6212687
TreVi1732721 <vincent2000.tremblay@videotron.ca>
2018-04-16 14:27:13 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -7,86 +7,107 @@ import java.util.Scanner;
  * Created by TreVi1732721 on 2018-02-26.
  */
 public class Main implements Serializable{
-    public static HashMap<String, Contact> listeContact = new HashMap<String, Contact>();
-    public static Contact nouveauContact = new Contact();
+
+    public static class WrongTypeExeption extends Exception{}
+
+    protected static HashMap<String, Contact> listeContact = new HashMap<String, Contact>();
+    protected static Contact nouveauContact = new Contact();
     public static void main(String[] args) {
 
         //new liste
 
-        boolean run = true;
+        boolean run = true, exeption = true;
         Scanner sc = new Scanner(System.in);
-        int nbContact = 0;
-        int choix = 0;
+        String choix = "0";
 
-        Charger();
        while(run){
-
+            exeption = true;
            System.out.println("Que voulez vous faire?" +
                    "\n1- Nouveau contact" +
                    "\n2- Afficher les contacts" +
                    "\n3- Modifier et ajouter un numero de téléphone" +
                    "\n4- Modifier un contact" +
-                   "\n5- effacer un contact");
-           switch(sc.nextInt()){
+                   "\n5- Effacer un contact" +
+                   "\n6- Charger la liste");
+
+           switch(sc.nextInt()) {
                case 1:
                    nouveauContact.NewContact();
-                   listeContact.put(nouveauContact.getPrenom(),nouveauContact);
-                   nbContact++;
+                   listeContact.put(nouveauContact.getPrenom(), nouveauContact);
                    break;
                case 2:
-                   for (int i=1; i<=nbContact; i++) {
-                       System.out.println("___________________________________");
-                       System.out.println("Contact "+ i );
-                       listeContact.get(i).PrintContact();
-                       System.out.println("\n___________________________________");
-                   }
+                   System.out.println("Quel est le contact à afficher?");
+                   try {
+                       choix = sc.next();
+                       for (int i = 0; i < choix.length(); i++) {
+                           if ((int) choix.toUpperCase().charAt(i) > 90 && (int) choix.toUpperCase().charAt(i) < 65) {
+                               throw new WrongTypeExeption();
+                           }
+                       }
 
+
+                       System.out.println("___________________________________");
+                       System.out.println("Contact " + choix);
+                       listeContact.get(choix).PrintContact();
+                       System.out.println("\n___________________________________");
+                   } catch (WrongTypeExeption one) {
+                       System.out.print("Entrée selement des lettres svp.");
+                   }
                    break;
                case 3:
                    System.out.println("Que voulez vous faire?" +
                            "\n 1- modifier un numero" +
                            "\n 2- ajouter un numero");
-                   choix = sc.nextInt();
-                   if(choix==1){
-                       System.out.println("modifier quel numero de contact?");
-                       choix = sc.nextInt();
+                   try{
+                   choix = sc.next();
+                   if(choix=="1"){
+                       System.out.println("modifier quel contact?");
+                       choix = sc.next();
                        listeContact.get(choix).ModTelephone();
-                   }else if(choix==2){
+                   }else if(choix=="2"){
                        System.out.println("Ajouter un numero a quel contact?");
-                       choix = sc.nextInt();
+                       choix = sc.next();
                        listeContact.get(choix).NewTelephone();
+                   }else {
+                       System.out.println("Selection invalide, veuiller faire un choix valide.");
+                   }}catch(Exception one){
+                       System.out.print("vous devez entrée une selection valide!");
                    }
                    break;
                case 4:
-                   System.out.println("Quel est le numero du contact à modifier?");
-                   choix = sc.nextInt();
+                   System.out.println("Quel est le contact à modifier?");
+                   choix = sc.next();
                    listeContact.get(choix).ModContact();
                    break;
                case 5:
                    System.out.print("Quel est le contact à effacer?");
-                   choix = sc.nextInt();
+                   choix = sc.next();
                    listeContact.remove(choix);
                    break;
+               case 6:
+                   Charger();
                default:
-                   break;
            }
-           System.out.print("voulez vous continuer la liste de contacts?" +
-                   "\n  1- oui" +
-                   "\n  2- non\n");
-           choix = sc.nextInt();
-           if (choix ==  2){
-               run = false;
-           }
+               Sauvegarder();
+               System.out.print("voulez vous continuer la liste de contacts?" +
+                       "\n  1- oui" +
+                       "\n  2- non \n");
+               if (sc.nextInt() == 2){
+                   run = false;
+               }
+
        }
-        Sauvegarder();
+       }
+
 
 
     }
     public static void Sauvegarder(){
         try {
-            ObjectOutputStream sortie = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("test.dat")));
-            sortie.writeObject(listeContact);
-            sortie.close();
+            ObjectOutputStream sortieListe = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("test.dat")));
+            sortieListe.writeObject(listeContact);
+            sortieListe.close();
+            System.out.println("Le programme a été enregistré !");
         }catch (Exception two){
             System.out.println("Le programme n'a pas été enregistré");
         }
@@ -96,6 +117,7 @@ public class Main implements Serializable{
             ObjectInputStream entree = new ObjectInputStream(new BufferedInputStream(new FileInputStream("test.dat")));
             listeContact = (HashMap<String, Contact>) entree.readObject();
             entree.close();
+            System.out.println("Le programme a été charger !");
         }catch(Exception one){
             System.out.println("Le programme n'a pas trouvé d'enregistrement");
         }
